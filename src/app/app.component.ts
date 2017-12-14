cf97258cb8e5e6f85d4bddf38a2f6bb2fa7fd8d6
Bhalchandra Bhosale <BHALCHANDRA.BHOSALE@pb.com>
2017-12-14 15:31:11 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Pitney Bowes';
+
+  ngOnInit() {
+    // Needed to "activate" all BS4 tooltips and popovers
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+  }
 }
